305b88a5237484862408b4014c8df2be4d6dbcf0
dgc1899 <daniel.garcia18@tectijuana.edu.mx>
2025-09-27 23:07:12 -0700

    add receiveAttack method

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,3 +1,5 @@
+import { Ship } from "../src/Ship.js";
+
 class Gameboard {
   #board;
 
@@ -9,7 +11,7 @@ class Gameboard {
     this.#board = [];
     for (let i = 0; i < 10; i++) {
       this.#board[i] = new Array(10);
-      this.#board[i].fill(0);
+      this.#board[i].fill({ ship: undefined, attacked: false });
     }
   }
 
@@ -18,7 +20,9 @@ class Gameboard {
   }
 
   #isCoordinateAlreadyOccupied(coord) {
-    if (this.#board[coord[0]][coord[1]] != 0) return true;
+    if (!Object.values(this.#board[coord[0]][coord[1]]).includes(undefined)) {
+      return true;
+    }
     return false;
   }
 
@@ -30,13 +34,53 @@ class Gameboard {
       for (let i = 0; i < ship.length; i++) {
         resultCoords.push([x, y + i]);
         if (!this.#isCoordinateAlreadyOccupied(resultCoords[i])) {
-          this.#board[x][y + i] = ship.length;
+          this.#board[x][y + i] = { ship: ship, attacked: false };
         } else {
           throw new Error("Space is already occupied!");
         }
       }
     }
     return resultCoords;
+  }
+
+  #areCoordinatesValid(x, y) {
+    const boundary = 10;
+    if (x >= 0 && x < boundary && y >= 0 && y < boundary) {
+      return true;
+    }
+    return false;
+  }
+
+  receiveAttack(x, y) {
+    if (!this.#areCoordinatesValid(x, y)) {
+      throw new Error("Coordinates are not valid");
+    }
+    const attackedCoord = this.#board[x][y];
+    let result = {};
+    if (attackedCoord["attacked"] == true) {
+      result = {
+        shipHit: undefined,
+        xCoord: undefined,
+        yCoord: undefined,
+      };
+    } else {
+      this.#board[x][y]["attacked"] = true;
+      if (attackedCoord["ship"] !== undefined) {
+        attackedCoord["ship"].hit(); //Hit the ship
+        result = {
+          shipHit: this.board[x][y]["ship"],
+          xCoord: x,
+          yCoord: y,
+        };
+      } else {
+        result = {
+          shipHit: undefined,
+          xCoord: x,
+          yCoord: y,
+        };
+      }
+    }
+    return result;
   }
 }
 
--- test/Gameboard.test.js
+++ test/Gameboard.test.js
@@ -10,7 +10,7 @@ beforeEach(() => {
 it("Test that the gameboard is initialized empty", () => {
   const board = new Gameboard();
   const isEmpty = board.board.every((row) =>
-    row.every((element) => element === 0),
+    row.every(() => ({ ship: undefined, isSunk: 0 })),
   );
   expect(isEmpty).toBe(true);
 });
@@ -59,4 +59,91 @@ it("Test place ship in board in an occupied position", () => {
   board.placeShip(mockShip, 5, 5);
 
   expect(() => board.placeShip(mockShip2, 5, 5)).toThrow();
+});
+
+it("Receive an attack in an empty cell", () => {
+  const board = new Gameboard();
+  const coords = [0, 0];
+  const expected = {
+    shipHit: undefined,
+    xCoord: 0,
+    yCoord: 0,
+  };
+
+  expect(board.receiveAttack(coords[0], coords[1])).toEqual(expected);
+});
+
+it("Receive an attack in an empty cell", () => {
+  const board = new Gameboard();
+  const coords = [9, 9];
+  const expected = {
+    shipHit: undefined,
+    xCoord: 9,
+    yCoord: 9,
+  };
+
+  expect(board.receiveAttack(coords[0], coords[1])).toEqual(expected);
+});
+
+it("Receive an attack in an empty cell", () => {
+  const board = new Gameboard();
+  const coords = [5, 4];
+  const expected = {
+    shipHit: undefined,
+    xCoord: 5,
+    yCoord: 4,
+  };
+
+  expect(board.receiveAttack(coords[0], coords[1])).toEqual(expected);
+});
+
+it("Receive an attack in an invalid cell", () => {
+  const board = new Gameboard();
+  const coords = [-1, 10];
+  expect(() => board.receiveAttack(coords[0], coords[1])).toThrow();
+});
+
+it("Receive an attack in a cell occupied by a ship", () => {
+  const board = new Gameboard();
+  Ship.mockImplementation((length) => ({ length, hit: jest.fn() }));
+  const ship = new Ship(3);
+  const expected = {
+    shipHit: ship,
+    xCoord: 0,
+    yCoord: 0,
+  };
+
+  board.placeShip(ship, 0, 0);
+  const actual = board.receiveAttack(0, 0);
+
+  expect(actual).toEqual(expected);
+});
+
+it("Receive an attack in a cell that was already attacked before", () => {
+  const board = new Gameboard();
+  const expected = {
+    shipHit: undefined,
+    xCoord: undefined,
+    yCoord: undefined,
+  };
+  board.receiveAttack(5, 5);
+
+  expect(board.receiveAttack(5, 5)).toEqual(expected);
+});
+
+it("Receive an attack in a cell occupied by a ship AND that has been attacked before", () => {
+  const board = new Gameboard();
+  Ship.mockImplementation((length) => ({ length, hit: jest.fn() }));
+  const mockShip = new Ship(5);
+  board.placeShip(mockShip, 2, 3);
+  board.receiveAttack(2, 5);
+
+  const actual = board.receiveAttack(2, 5);
+  const expected = {
+    shipHit: undefined,
+    xCoord: undefined,
+    yCoord: undefined,
+  };
+
+  expect(actual).toEqual(expected);
 });
